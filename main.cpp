81f00ac518a3f115c34599680032ce0c86a487bc
Alexandre Philibert <alexandre.philibert@ik.me>
2023-01-09 16:54:48 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// -----------------------------------------------------------------------------------------------
+// Fichier        : main.cpp
+// Auteur(s)      : DURGERDIL Noam & PHILIBERT Alexandre
+// Date           : 2022-01-09
+// But            :
+// Modifications  : NIL
+// Remarque(s)    :
+// Compilateur    : g++ 11.2.0
+// Standard C++   : C++ 20
+// -----------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+   std::cout << "Hello, World!" << std::endl;
+
+   return EXIT_SUCCESS;
+}
